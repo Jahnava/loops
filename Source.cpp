7d01787ead7c0ad8ba357b91f0841e2f615eba63
Jahnava <jahnavalmt@gmail.com>
2020-09-29 10:16:36 -0400

    Addeda while loop that repeats five time and evaluates the sun of the 5 numbers

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,25 @@
-//ouygoh
+//
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//variables
+	int counter = 0;
+	int sum = 0;
+	int inputNum;
+
+	while (counter < 5)
+	{
+		cout << "Enter a number: ";
+		cin >> inputNum;
+
+		sum += inputNum;
+		counter++;
+
+	}
+
+	cout << "The sum of the value is: " << sum << endl;
+
+}
